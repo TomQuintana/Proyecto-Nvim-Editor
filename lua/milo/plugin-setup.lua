b30fd4f3a0873f0899d8925013f2b79bb73592da
tomQuintana <tomquintana20@gmail.com>
2023-02-10 19:37:28 -0300

    config for treesitter plug

--- lua/milo/plugin-setup.lua
+++ lua/milo/plugin-setup.lua
@@ -35,6 +35,15 @@ return packer.startup(function(use)
   -- Theme
   use("folke/tokyonight.nvim")
 
+  -- Tresitter 
+      use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
+
  if packer_bootstrap then
     require("packer").sync()
   end
