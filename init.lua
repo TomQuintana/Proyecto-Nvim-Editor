e14f75478dde3a9df11bb46e7b81ea4cdaeac9b2
tomQuintana <tomquintana20@gmail.com>
2023-03-01 09:41:45 -0300

    add colors for indentantion like vsc

--- init.lua
+++ init.lua
@@ -2,20 +2,22 @@ require("milo.core.keymaps")
 require("milo.core.options")
 require("milo.core.colorschema")
 require("milo.plugin-setup")
-require("milo.plugins.autopairs")
-require("milo.plugins.tresitter") --NOTE: see more docu of this plug
-require("milo.plugins.gitvim")
-require("milo.plugins.telescope")
-require("milo.plugins.tree")
-require("milo.plugins.statusbar")
-require("milo.plugins.bufferline")
 require("milo.plugins.nvim-cmp") --NOTE: see more docu of this plug
 require("milo.plugins.lsp.mason") --NOTE: see more docu of this plug
 require("milo.plugins.lsp.lspsaga") --NOTE: see more docu of this plug
 require("milo.plugins.lsp.lspconfig") --NOTE: see more docu of this plug
+require("milo.plugins.icons")
+require("milo.plugins.tree")
+require("milo.plugins.tresitter") --NOTE: see more docu of this plug
+require("milo.plugins.telescope")
+require("milo.plugins.autopairs")
+require("milo.plugins.gitvim")
+require("milo.plugins.telescope")
+require("milo.plugins.statusbar")
+require("milo.plugins.bufferline")
 require("milo.plugins.todocomments")
 require("milo.plugins.comments")
 require("milo.plugins.mergeConflict")
 require("milo.plugins.markdownPreview")
 require("milo.plugins.trouble")
-require("milo.plugins.icons")
+require("milo.plugins.indentation")
