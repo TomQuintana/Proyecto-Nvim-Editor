62b1c6f6d53a8f2db76040f67777de7591a62ca9
tomQuintana <tomquintana20@gmail.com>
2023-03-04 22:59:08 -0300

    add stily and color for indentline

--- lua/milo/plugins/indentation.lua
+++ lua/milo/plugins/indentation.lua
@@ -1,23 +1,50 @@
-vim.opt.termguicolors = true
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E5C07B  gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guifg=#98C379 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
-
--- vim.opt.list = true
--- vim.opt.listchars:append "space:⋅"
--- vim.opt.listchars:append "eol:↴"
+-- vim.opt.termguicolors = true
+-- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E5C07B  gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#98C379 gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
 --
+-- -- vim.opt.list = true
+-- -- vim.opt.listchars:append "space:⋅"
+-- -- vim.opt.listchars:append "eol:↴"
+-- --
+-- require("indent_blankline").setup {
+--   show_current_context = true,
+--
+--   show_current_context_start = false,
+--     char_highlight_list = {
+--
+--         "IndentBlanklineIndent1",
+--         "IndentBlanklineIndent2",
+--     },
+--    space_char_highlight_list = {
+--         "IndentBlanklineIndent1",
+--         "IndentBlanklineIndent2",
+--     },
+-- }indent_blankline_char = '|'
+--
+vim.opt.termguicolors = true
+vim.g.indent_blankline_char = '┆'
+vim.g.indent_blankline_char_blankline = ' '
+vim.g.indent_blankline_space_char_highlight_list = 'Function'
+vim.cmd [[ highlight IndentBlanklineContextSpaceChar guifg=#F0F25F ]]
+vim.g.indent_blankline_show_current_context = true
+
+--vim.cmd [[highlight IndentBlanklineIndent1 guifg=#56B6C2 ]]
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#56B6C2 ]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#F0F25F ]]
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#FDDB88 ]]
+vim.cmd [[highlight IndentBlanklineContextChar guifg=#F4A0E6 ]]
+
 require("indent_blankline").setup {
-  show_current_context = true,
-  show_current_context_start = true,
-    char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-    },
-   space_char_highlight_list = {
-        "IndentBlanklineIndent1",
-        "IndentBlanklineIndent2",
-    },
+  indent_blankline_char_blankline = ' ',
+  char_highlight_list = {
+    "IndentBlanklineIndent1",
+    "IndentBlanklineIndent2",
+    "IndentBlanklineIndent3",
+    "IndentBlanklineContextChar"
+  },
 }
+
+--TODO: choice correct collors for lines
