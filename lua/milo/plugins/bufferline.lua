15c38e363e4dcce7a636dbd6fb23206a4a5cacba
tomQuintana <tomquintana20@gmail.com>
2023-03-08 20:01:49 -0300

    put icon hint

--- lua/milo/plugins/bufferline.lua
+++ lua/milo/plugins/bufferline.lua
@@ -13,7 +13,7 @@ buffer.setup({
     close_icon = '',
     diagnostics = "nvim_lsp",
     diagnostics_indicator = function(count, level, diagnostics_dict, context)
-      local icon = level:match("error") and " " or " "
+      local icon = level:match("error") and " " or " "
       return " " .. icon .. count
     end
   }
